6e611f3653a183f20272471f1464279bd7635d4a
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-02-24 21:58:06 +0500

    login page ui completed

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Tooltip } from 'antd';
 
 export default function Login() {
     return (
         <div>
-            <section className="vh-100">
+            <section className="d-flex flex-column justify-content-between loginSection">
                 <div className="container-fluid h-custom">
                     <div className="row d-flex justify-content-center align-items-center h-100">
                         <div className="col-md-9 col-lg-6 col-xl-5">
@@ -16,57 +17,41 @@ export default function Login() {
                         <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
                             <form>
                                 <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                                    <p className="lead fw-normal mb-0 me-3">Sign in with</p>
-                                    <button type="button" className="btn btn-primary btn-floating mx-1">
-                                        <i className="fab fa-facebook-f" />
-                                    </button>
-                                    <button type="button" className="btn btn-primary btn-floating mx-1">
-                                        <i className="fab fa-twitter" />
-                                    </button>
-                                    <button type="button" className="btn btn-primary btn-floating mx-1">
-                                        <i className="fab fa-linkedin-in" />
-                                    </button>
+                                    <p className="lead fw-normal mb-0 me-3">Sign in with:</p>
+                                    <Tooltip title="Google">
+                                        <button type="button" className="btn btn-primary btn-floating mx-1 rounded-circle">
+                                            <i class="fa-brands fa-google" />
+                                        </button>
+                                    </Tooltip>
                                 </div>
-                                <div className="divider d-flex align-items-center my-4">
-                                    <p className="text-center fw-bold mx-3 mb-0">Or</p>
+                                <div className="divider my-4">
+                                    <hr class="hr-text" data-content="OR" />
                                 </div>
                                 {/* Email input */}
                                 <div className="form-outline mb-4">
+                                    <label className="form-label" htmlFor="form3Example3">
+                                        Email address
+                                    </label>
                                     <input
                                         type="email"
                                         id="form3Example3"
                                         className="form-control form-control-lg"
                                         placeholder="Enter a valid email address"
                                     />
-                                    <label className="form-label" htmlFor="form3Example3">
-                                        Email address
-                                    </label>
                                 </div>
                                 {/* Password input */}
                                 <div className="form-outline mb-3">
+                                    <label className="form-label" htmlFor="form3Example4">
+                                        Password
+                                    </label>
                                     <input
                                         type="password"
                                         id="form3Example4"
                                         className="form-control form-control-lg"
                                         placeholder="Enter password"
                                     />
-                                    <label className="form-label" htmlFor="form3Example4">
-                                        Password
-                                    </label>
                                 </div>
                                 <div className="d-flex justify-content-between align-items-center">
-                                    {/* Checkbox */}
-                                    <div className="form-check mb-0">
-                                        <input
-                                            className="form-check-input me-2"
-                                            type="checkbox"
-                                            defaultValue=""
-                                            id="form2Example3"
-                                        />
-                                        <label className="form-check-label" htmlFor="form2Example3">
-                                            Remember me
-                                        </label>
-                                    </div>
                                     <a href="#!" className="text-body">
                                         Forgot password?
                                     </a>
@@ -90,26 +75,19 @@ export default function Login() {
                         </div>
                     </div>
                 </div>
-                <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+                <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary align-items-center">
                     {/* Copyright */}
                     <div className="text-white mb-3 mb-md-0">
-                        Copyright © 2020. All rights reserved.
+                        Copyright © 2024. All rights reserved.
                     </div>
                     {/* Copyright */}
                     {/* Right */}
-                    <div>
-                        <a href="#!" className="text-white me-4">
-                            <i className="fab fa-facebook-f" />
-                        </a>
-                        <a href="#!" className="text-white me-4">
-                            <i className="fab fa-twitter" />
-                        </a>
-                        <a href="#!" className="text-white me-4">
-                            <i className="fab fa-google" />
-                        </a>
-                        <a href="#!" className="text-white">
-                            <i className="fab fa-linkedin-in" />
-                        </a>
+                    <div className='border px-2 py-1 rounded'>
+                        <Tooltip title={"Google"}>
+                            <a href="#!" className="text-white">
+                                <i className="fab fa-google" />
+                            </a>
+                        </Tooltip>
                     </div>
                     {/* Right */}
                 </div>
