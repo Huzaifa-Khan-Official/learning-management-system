d9cda6a04deafcd40f6a800e0185f671844d4cb1
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-02-25 22:12:25 +0500

    registeration form ui done

--- src/Components/AdmissionForm.jsx
+++ src/Components/AdmissionForm.jsx
@@ -63,10 +63,12 @@ export default function AdmissionForm() {
                                                     <label className="form-label">
                                                         Contact Number:
                                                     </label>
-                                                    <input
-                                                        type="text"
+                                                    <PatternFormat
+                                                        valueIsNumericString
+                                                        format="###########"
+                                                        mask="_"
                                                         className="form-control form-control-lg"
-                                                        placeholder='Enter your contact number'
+                                                        value={"03"}
                                                     />
                                                 </div>
                                             </div>
@@ -76,7 +78,7 @@ export default function AdmissionForm() {
                                             <div className="col-md-6 mb-4 d-flex align-items-center">
                                                 <div className="form-outline datepicker w-100">
                                                     <label className="form-label">
-                                                        Cnic
+                                                        CNIC
                                                     </label>
                                                     <PatternFormat
                                                         valueIsNumericString
@@ -100,7 +102,75 @@ export default function AdmissionForm() {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="mt-2 pt-2">
+                                        <div className="row">
+                                            {/* select gender input */}
+                                            <div className="col-md-6 mb-4 d-flex align-items-center">
+                                                <div className="form-outline w-100">
+                                                    <label className='form-label'>Select Gender:</label>
+                                                    <select class="form-select" aria-label="Default select example">
+                                                        <option selected disabled>Select Gender</option>
+                                                        <option value="Male">Male</option>
+                                                        <option value="Female">Female</option>
+                                                        <option value="Other">Other</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            {/* select education input */}
+                                            <div className="col-md-6 mb-4 d-flex align-items-center">
+                                                <div className="form-outline w-100">
+                                                    <label className='form-label'>Last Qualification:</label>
+                                                    <select class="form-select" aria-label="Default select example">
+                                                        <option selected disabled>Last Qualification</option>
+                                                        <option value="Matriculation">Matriculation</option>
+                                                        <option value="Intermediate">Intermediate</option>
+                                                        <option value="Under Graduate">Under Graduate</option>
+                                                        <option value="Graduate">Graduate</option>
+                                                        <option value="PhD">PhD</option>
+                                                        <option value="Other">Other</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-12 mb-4 d-flex align-items-center">
+                                                <div className="form-outline w-100">
+                                                    <label className='form-label'>Address:</label>
+                                                    <input type="text" className="form-control" placeholder='Address' />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div class="col-12 mb-4 d-flex align-items-center">
+                                                <div className="form-outline w-100">
+                                                    <label className='form-label'>Do you have a Laptop?</label>
+                                                    <select class="form-select" aria-label="Default select example">
+                                                        <option selected disabled>Do you have a Laptop?</option>
+                                                        <option value="Yes">Yes</option>
+                                                        <option value="No">No</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="row">
+                                            <div className="col-12 mb-4 d-flex align-items-center">
+                                                <div className="form-outline w-100">
+                                                    <label className='form-label'>Picture</label>
+                                                    <div className="d-flex">
+                                                        <div class="profilePicDiv">
+                                                            {/* <img src="https://avatars.githubusercontent.com/u/122217933?v=4" alt="" id='profileImg' /> */}
+                                                        </div>
+                                                        <div class="uploadBtnDiv">
+                                                            <div class="inputDiv" id="picInputDiv">
+                                                                <input type="file" accept="image/*" id="picInput" />
+                                                                <span>+ Upload</span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="pt-2">
                                             <button
                                                 className="btn btn-primary btn-lg fs-6"
                                                 type='submit'
